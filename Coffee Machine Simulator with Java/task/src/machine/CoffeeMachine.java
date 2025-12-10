00f141dd55209e5a7322292fe1160fe3c9c9cce5
Vladislav <vn1415843@gmail.com>
2025-12-10 12:50:49 +0200

    step-3 finished

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -1,5 +1,8 @@
 package machine;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.Scanner;
 
 public class CoffeeMachine {
@@ -12,7 +15,6 @@ public class CoffeeMachine {
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
 
-        //example 1
         Scanner sc = new Scanner(System.in);
         System.out.println("Write how many cups of coffee you will need: ");
         int cups = sc.nextInt();
@@ -21,10 +23,52 @@ public class CoffeeMachine {
         int milk = 50 * cups;
         int coffeeBeans = 15 * cups;
 
-        System.out.println("For " + cups + " of coffee you will need: \n" + water + " ml of water \n"  + milk + " ml of milk \n" + coffeeBeans + " g of coffee beans.");
+        System.out.println("For " + cups + " of coffee you will need: \n" + water + " ml of water \n" + milk + " ml of milk \n" + coffeeBeans + " g of coffee beans.");
 
-        //example2
 
+        var coffee = new Coffee(water, milk, coffeeBeans);
+        checkCoffee(
+                coffee
+        );
 
     }
+
+    public static void checkCoffee(Coffee coffee) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int water = sc.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milk = sc.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int grams = sc.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
+        int cups = sc.nextInt();
+
+        final int WATER = 200;
+        final int MILK = 50;
+        final int COFFEEBEANS = 15;
+
+        int maxCupsByWater = water / WATER;
+        int maxMilkByMilk = milk / MILK;
+        int maxCoffeeBeans = grams / COFFEEBEANS;
+
+        int maxCups = Math.min(maxCupsByWater, Math.min(maxMilkByMilk, maxCoffeeBeans));
+
+        if (maxCups == cups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (maxCups > cups) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (maxCups - cups) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + maxCups + " cup(s) of coffee");
+        }
+    }
+}
+
+@Data
+@AllArgsConstructor
+class Coffee {
+    private int water;
+    private int milk;
+    private int coffeeBeans;
+
 }
--- Coffee Machine Simulator with Java/task/test/CoffeeMachineTest.java
+++ Coffee Machine Simulator with Java/task/test/CoffeeMachineTest.java
@@ -4,74 +4,112 @@ import org.hyperskill.hstest.testcase.TestCase;
 
 import java.util.List;
 
-public class CoffeeMachineTest extends StageTest<String> {
+
+class TestClue {
+    boolean cond;
+    int quantity;
+    boolean showOutputs;
+
+    TestClue(boolean cond, int quantity, boolean showOutputs) {
+        this.cond = cond;
+        this.quantity = quantity;
+        this.showOutputs = showOutputs;
+    }
+}
+
+public class CoffeeMachineTest extends StageTest<TestClue> {
     @Override
-    public List<TestCase<String>> generate() {
+    public List<TestCase<TestClue>> generate() {
         return List.of(
-            new TestCase<String>()
-                .setInput("25")
-                .setAttach("25"),
+                new TestCase<TestClue>().setAttach(new TestClue(true, 0, true)).setInput("300\n65\n111\n1"),
 
-            new TestCase<String>()
-                .setInput("125")
-                .setAttach("125"),
+                new TestCase<TestClue>().setAttach(new TestClue(true, 2, true)).setInput("600\n153\n100\n1"),
 
-            new TestCase<String>()
-                .setInput("1")
-                .setAttach("1"),
+                new TestCase<TestClue>().setAttach(new TestClue(true, 2, true)).setInput("1400\n150\n100\n1"),
 
-            new TestCase<String>()
-                .setInput("123")
-                .setAttach("123")
+                new TestCase<TestClue>().setAttach(new TestClue(true, 2, true)).setInput("1400\n1500\n45\n1"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(false, 2, true)).setInput("599\n250\n200\n10"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(true, 867, true)).setInput("345640\n43423\n23234\n1"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(false, 1548, true)).setInput("345640\n434230\n23234\n19246"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(false, 172, true)).setInput("34564\n43423\n23234\n19246"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(true, 0, false)).setInput("399\n112\n111\n1"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(true, 3, false)).setInput("2400\n249\n100\n1"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(true, 1, false)).setInput("1400\n1500\n44\n1"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(false, 2, false)).setInput("500\n250\n200\n10"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(true, 171, false)).setInput("34564\n43423\n23234\n1"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(true, 1547, false)).setInput("345640\n434230\n23234\n1"),
+
+                new TestCase<TestClue>().setAttach(new TestClue(false, 868, false)).setInput("345640\n43423\n23234\n19246")
         );
     }
 
     @Override
-    public CheckResult check(String reply, String clue) {
-        String[] lines = reply.split("\\n");
-        if (lines.length < 4) {
-            return CheckResult.wrong("The output contains " + lines.length + " lines, but it should contain 4.");
+    public CheckResult check(String reply, TestClue clue) {
+        String[] lines = reply.trim().split(":");
+
+        if (lines.length <= 1) {
+            return CheckResult.wrong("Looks like you didn't print anything!");
+        } else if (lines.length <= 4) {
+            return CheckResult.wrong("Looks like you didn't print the final result!");
         }
-        String[] last3Lines = {
-                lines[lines.length - 3],
-                lines[lines.length - 2],
-                lines[lines.length - 1]
-        };
 
-        int cups = Integer.parseInt(clue);
-        boolean hasWater = false, hasMilk = false, hasCoffeeBeans = false;
+        String userOutput = lines[lines.length - 1].trim();
+        String userOutputLowerCase = userOutput.toLowerCase();
+        boolean ans = clue.cond;
+        int quantity = clue.quantity;
+        if (ans) {
+            if (quantity > 0) {
+                boolean isCorrect = userOutputLowerCase.contains(Integer.toString(quantity)) && userOutputLowerCase.contains("yes");
 
-        for (String line : last3Lines) {
-            line = line.toLowerCase();
+                if (isCorrect) {
+                    return CheckResult.correct();
+                } else {
+                    String correctOutput = "Yes, I can make that amount of coffee" + " (and even " + quantity + " more than that)";
 
-            if (line.contains("water")) {
-                hasWater = line.contains(Integer.toString(cups * 200));
-                if (!hasWater) {
-                    return CheckResult.wrong("For the input: " + clue + " cups of coffee\nYour program output: " + line + "\nBut the required amount of water is " + (cups * 200) + " ml");
+                    if (clue.showOutputs) {
+                        return CheckResult.wrong("Your output:\n" + userOutput + "\nCorrect output:\n" + correctOutput);
+                    } else {
+                        return CheckResult.wrong("");
+                    }
                 }
-            } else if (line.contains("milk")) {
-                hasMilk = line.contains(Integer.toString(cups * 50));
-                if (!hasMilk) {
-                    return CheckResult.wrong("For the input: " + clue + " cups of coffee\nYour program output: " + line + "\nBut the required amount of milk is " + (cups * 50) + " ml");
+            }
+
+            String correctOutput = "Yes, I can make that amount of coffee";
+
+            if (userOutputLowerCase.contains(correctOutput.toLowerCase())) {
+                return CheckResult.correct();
+            } else {
+                if (clue.showOutputs) {
+                    return CheckResult.wrong("Your output:\n" + userOutput + "\nCorrect output:\n" + correctOutput);
+                } else {
+                    return CheckResult.wrong("");
                 }
-            } else if (line.contains("coffee bean")) {
-                hasCoffeeBeans = line.contains(Integer.toString(cups * 15));
-                if (!hasCoffeeBeans) {
-                    return CheckResult.wrong("For the input: " + clue + " cups of coffee\nYour program output: " + line + "\nBut the required amount of coffee beans is " + (cups * 15) + " g");
+            }
+
+        } else {
+            boolean isCorrect = userOutputLowerCase.contains("no") && userOutputLowerCase.contains(Integer.toString(quantity));
+
+            if (isCorrect) {
+                return CheckResult.correct();
+            } else {
+                String correctOutput = "No, I can make only " + quantity + " cup(s) of coffee";
+
+                if (clue.showOutputs) {
+                    return CheckResult.wrong("Your output:\n" + userOutput + "\nCorrect output:\n" + correctOutput);
+                } else {
+                    return CheckResult.wrong("");
                 }
             }
         }
-
-        if (!hasWater) {
-            return CheckResult.wrong("There is no line specifying the required amount of \"water\".");
-        }
-        if (!hasMilk) {
-            return CheckResult.wrong("There is no line specifying the required amount of \"milk\".");
-        }
-        if (!hasCoffeeBeans) {
-            return CheckResult.wrong("There is no line specifying the required amount of \"coffee beans\".");
-        }
-
-        return CheckResult.correct();
     }
 }
